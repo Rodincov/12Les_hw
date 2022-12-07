5ee655675335aa2ce91f2e7f67c32e63794ab65f
Rodincov <rod1ncov1488@gmail.com>
2022-12-07 16:22:41 +0600

    dz4

--- main.js
+++ main.js
@@ -9,18 +9,25 @@ document.body.append(wrapper)
 let count = 1
 button.addEventListener('click', () => {
     if(count % 2 === 0) {
+        addGray()
+    } else {
+        addRed()
+    }})
+
+
+    
+    function addGray() {
         let card = document.createElement('div')
         card.classList.add('card-gray')
         wrapper.append(card)
-        
         card.innerHTML = count
         count += 1
-    } else {
+    }
+    function addRed() {
         let card = document.createElement('div')
         card.classList.add('card-red')
         wrapper.append(card)
         card.innerHTML = count
         count += 1
-    }})
-
+    }
 
